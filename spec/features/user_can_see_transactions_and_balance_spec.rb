eef95972c432391cc3d324da5c6295e20cea43be
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-07-29 15:09:47 +0100

    test: remove expectation that print_statement header includes date

--- spec/features/user_can_see_transactions_and_balance_spec.rb
+++ spec/features/user_can_see_transactions_and_balance_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'User can view transaction history on bank statement print out' d
     account.withdraw(600)
     account.deposit(100)
 
-    header = "date || credit || debit || balance"
+    header = "credit || debit || balance"
     first = "1000 || || 1000"
     second = " || 600 || 400"
     third = "100 || || 500"
