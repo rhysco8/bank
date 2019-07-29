f3e4a0fedada2b78f3dc32d7f08b52e4b21f157f
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-07-29 13:51:58 +0100

    test: add unit test for withdrawing

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,15 +1,24 @@
 require 'bank_account'
 
 describe BankAccount do
+
+  before(:each) do
+    @account = described_class.new
+  end
+
   describe '#deposit' do
     it 'increases balance by 1' do
-      account = BankAccount.new
-      expect { account.deposit(1) }.to change { account.balance }.by(1)
+      expect { @account.deposit(1) }.to change { @account.balance }.by(1)
     end
 
-    it "increases balance by 100" do
-      account = BankAccount.new
-      expect { account.deposit(100) }.to change { account.balance }.by(100)  
+    it 'increases balance by 100' do
+      expect { @account.deposit(100) }.to change { @account.balance }.by(100)
+    end
+  end
+
+  describe '#withdraw' do
+    it 'decreases balance by 1' do
+      expect { @account.withdraw(1) }.to change { @account.balance }.by(-1)
     end
   end
 end
